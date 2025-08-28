3ace40ff6a3cf197e4e57151834b4786a4785b6d
Natali Shatina <natali.shatina@gmail.com>
2025-08-28 13:48:59 +0200

    hw done

--- js/task-4.js
+++ js/task-4.js
@@ -1,27 +1,24 @@
 const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault(); // сторінка не перезавантажується
+  event.preventDefault();
 
   const { email, password } = form.elements;
 
-  // Очищаємо пробіли по краях
   const emailValue = email.value.trim();
   const passwordValue = password.value.trim();
 
-  // Перевірка на порожні поля
   if (emailValue === "" || passwordValue === "") {
     alert("All form fields must be filled in");
     return;
   }
 
-  // Створюємо об'єкт з даними форми
   const formData = {
     email: emailValue,
     password: passwordValue,
   };
 
-  console.log(formData); // вивід в консоль
+  console.log(formData);
 
-  form.reset(); // очищаємо форму
+  form.reset();
 });
